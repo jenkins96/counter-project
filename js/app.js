872d203bf4810e99b92343b224db707013a88c66
jenkins96 <adrianjnkns@gmail.com>
2021-02-21 21:15:54 +0000

    upload

--- js/app.js
+++ js/app.js
@@ -1,28 +1,9 @@
-(function(){
-  const buttons = document.querySelectorAll('.counterBtn')
-  let count= 0
+(function() {
+const buttons = document.querySelectorAll("button");
+buttons.addEventListener("click", function(){
+    document.body.style.background = "red";
+})
+})
 
 
-  //Add event listeners and functionailty to each button  
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(){
-      if (button.classList.contains('prevBtn')){
-        count--
-      } else if (button.classList.contains('nextBtn')){
-        count++
-      }
-
-      //Select the counter text
-      const counter = document.querySelector('#counter')
-      counter.textContent = count
-
-      if (count < 0 ){
-        counter.style.color = 'red'
-      } else if (count > 0){
-        counter.style.color = 'green'
-      } else {
-        counter.style.color = '#333333'
-      }
-    })
-  })
-})()
+})();
