baeb18c6ab02801be1e7d4c8bc92a78e8a1fea6a
jenkins96 <59536240+jenkins96@users.noreply.github.com>
2021-02-22 09:58:22 -0600

    fixing code

--- js/app.js
+++ js/app.js
@@ -22,26 +22,26 @@ buttons.forEach((element) => {
 // Having some trouble leaving value = 0 in default color :(
 
 (function() {
-    // Declaration of buttons and counter
-const buttons = document.querySelectorAll("button");
-const counter = document.getElementById("counter");
-// Using forEach()
-buttons.forEach((element) => {
-    // Event Listener
-    element.addEventListener("click", function(){
-      let add = counter++;
-      let substract = counter--;
-       if(element.innerHTML ==== "add count"){
-        add;
-       } else if(count > 0){
-            counter.style.background = "green";
-       } else if(element.innerHTML ==== "add count"){
-             substract;
-       }else{
-           counter.style.background = "red";
-       }
-      // Setting positive values green and negatives values red
-      //counter.innerHTML > 0 ? counter.style.color = "green": counter.style.color = "red";
-    }) 
-});
+  const buttons = document.querySelectorAll("button");
+  let count = 0;
+  buttons.forEach((element) => {
+  element.addEventListener("click", function() {
+  if(element.classList.contains("prevBtn")){
+  count--
+  }else if(element.classList.contains("nextBtn"){
+  count++
+  })
+  const counter = document.getElementById("counter")
+  counter.innerHTML = count
+      
+   if(counter > 0){
+   counter.style.color = "green";
+   }else if(counter < 0){
+   counter.style.color = "red";
+   }else {
+   counter.style.color = "#333333"
+   }
+      
+  })
+  })
 })();
